30c128f3924fb90e01addd0c01f32375058d8987
dalilafridi <dalilafridi@gmail.com>
2026-02-23 23:40:23 -0600

    Switch from DatabasePad to Supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-// Initialize database client
-const supabaseUrl = 'https://tsdymklssbphztnrvzom.databasepad.com';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImViZmUxMzRmLWYwODYtNDEyZi1iNjhiLTBkMDIyOWMyZjc2NSJ9.eyJwcm9qZWN0SWQiOiJ0c2R5bWtsc3NicGh6dG5ydnpvbSIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzcxODY5Njc2LCJleHAiOjIwODcyMjk2NzYsImlzcyI6ImZhbW91cy5kYXRhYmFzZXBhZCIsImF1ZCI6ImZhbW91cy5jbGllbnRzIn0.VVj2eGw41P9DYfZrBpW7Ui6EUZPbsL66VSsIl8i17c0';
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-
-export { supabase };
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
